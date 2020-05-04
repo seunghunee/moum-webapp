5c7dc809f5c9f55f7f8c8d8269ebf6c19e9a019b
Shin Seunghun <seunghunee@gmail.com>
2020-05-04 22:48:55 +0900

    Add: 기본 페이지 추가

---
+++ src/pages/ArticlePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ArticlePage() {
+  return <h1>Article</h1>;
+}
+
+export default ArticlePage;
---
+++ src/pages/EditPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditPage() {
+  return <h1>Edit</h1>;
+}
+
+export default EditPage;
