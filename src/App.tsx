7fb15fef43f6eeef88004e299e6295fda08522ec
Shin Seunghun <seunghunee@gmail.com>
2020-05-09 10:41:40 +0900

    Add: Material-UI의 CSS Baseline 적용
    각 브라우저별 다른 스타일을 통일하기 위해 적용했다.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import BodyCon from "./components/BodyCon";
 import Header from "./components/Header";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const App: React.FC = () => {
   return (
     <>
+      <CssBaseline />
       <Header />
       <BodyCon />
     </>
