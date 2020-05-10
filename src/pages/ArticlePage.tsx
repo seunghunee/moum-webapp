f97330f811b71187d6c384a6e0551fc482fb8efc
Shin Seunghun <seunghunee@gmail.com>
2020-05-10 16:27:39 +0900

    Add: ArticlePage, Article의 레이아웃을 만듬

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Article from "../components/Article";
 
 const ArticlePage: React.FC = () => {
-  return <h1>Article</h1>;
+  const classes = useStyle();
+
+  return (
+    <main className={classes.layout}>
+      <Article />
+    </main>
+  );
 };
+
+const useStyle = makeStyles((theme) => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    [theme.breakpoints.up(800 + theme.spacing(2) * 2)]: {
+      width: 800,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+}));
 
 export default ArticlePage;
