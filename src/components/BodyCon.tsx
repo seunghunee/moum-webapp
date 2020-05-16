e91a8c28e6edc914c0dbd07bf6ae807c9097f775
Shin Seunghun <seunghunee@gmail.com>
2020-05-16 21:14:35 +0900

    Delete: BodyCon에 있는 BrowserRouter를 제거
    루트 컴포넌트를 이미 감싸고 있기 때문에 불필요하다.

--- src/components/BodyCon.tsx
+++ src/components/BodyCon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import EditPage from "../pages/EditPage";
@@ -7,19 +7,17 @@ import ArticlePage from "../pages/ArticlePage";
 
 const BodyCon: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/article">
-          <ArticlePage />
-        </Route>
-        <Route path="/edit">
-          <EditPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/article">
+        <ArticlePage />
+      </Route>
+      <Route path="/edit">
+        <EditPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
   );
 };
 
