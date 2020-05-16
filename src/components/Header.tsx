8af679458932194f9f9269dfd9159385c75d7f8b
Shin Seunghun <seunghunee@gmail.com>
2020-05-16 21:40:08 +0900

    Add: 앱 타이틀에 홈페이지로의 링크를 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
 const Header: React.FC = () => {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <Typography variant="h5">moum</Typography>
+        <Link
+          component={RouterLink}
+          to="/"
+          variant="h5"
+          color="inherit"
+          underline="none"
+        >
+          moum
+        </Link>
       </Toolbar>
     </AppBar>
   );
