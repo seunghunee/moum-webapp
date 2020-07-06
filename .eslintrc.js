4ea7a9f6922dfd1da32f9d2a793dbb3aaa923761
Shin Seunghun <seunghunee@gmail.com>
2020-07-06 20:43:37 +0900

    Add: eslint에 no-non-null-assertion룰을 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
